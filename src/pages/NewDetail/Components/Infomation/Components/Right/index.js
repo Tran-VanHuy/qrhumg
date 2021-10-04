a8d95def415ecbb686c85c36f207c1d5d3b131d6
Văn Huy Trần <huy1821051049@gmail.com>
2021-10-04 22:49:16 +0700

    update

--- src/pages/NewDetail/Components/Infomation/Components/Right/index.js
+++ src/pages/NewDetail/Components/Infomation/Components/Right/index.js
@@ -1,15 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactHtmlParser from "react-html-parser";
 import "./style.css";
 import { FacebookShareButton, TwitterShareButton } from "react-share";
 import { CopyToClipboard } from "react-copy-to-clipboard";
 import toast from "react-hot-toast";
+import { Button, Modal } from "react-bootstrap";
+import QRCode from "qrcode.react";
 
 const RightInfomation = ({ data, slug }) => {
-  const onSubmitCopy = () => {
-    toast.success('Coppy link thành công')
-    alert("Copy thành công");
+  const [show, setShow] = useState(false);
 
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
+  const onSubmitCopy = () => {
+    toast.success("Coppy link thành công");
+    alert("Copy thành công");
   };
 
   return (
@@ -56,7 +62,28 @@ const RightInfomation = ({ data, slug }) => {
             Twitter
           </TwitterShareButton>
         </div>
-        <div className="item-share">QR</div>
+        <div className="item-share" variant="primary" onClick={handleShow}>
+          QR
+        </div>
+        <Modal
+          show={show}
+          onHide={handleClose}
+          backdrop="static"
+          keyboard={false}
+        >
+          <Modal.Header closeButton>
+            <Modal.Title>Modal title</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            <QRCode
+              id="qrcode"
+              value={`http://localhost:3000/tin-tuc/${slug}`}
+              size={250}
+              level={"H"}
+              includeMargin={true}
+            />
+          </Modal.Body>
+        </Modal>
         <div className="item-share" onClick={() => onSubmitCopy()}>
           <CopyToClipboard text={`http://localhost:3000/tin-tuc/${slug}`}>
             <div>Copy link</div>
